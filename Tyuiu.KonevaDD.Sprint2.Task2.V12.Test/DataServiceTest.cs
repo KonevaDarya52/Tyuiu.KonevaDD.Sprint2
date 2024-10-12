b5f133619e22dc8de9b409a7225563072e3a220a
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-10-12 14:41:27 +0500

    Task2

---
+++ Tyuiu.KonevaDD.Sprint2.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.KoneveDD.Sprint2.Task2.V12.Lib;
+namespace Tyuiu.KoneveDD.Sprint2.Task2.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 7;
+            int y = 6;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
