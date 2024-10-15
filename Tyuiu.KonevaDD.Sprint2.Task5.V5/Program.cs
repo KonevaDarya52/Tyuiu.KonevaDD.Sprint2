04d715881fa3895e190979c5715d12bedc16a15e
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-10-15 15:54:36 +0500

    Task5

---
+++ Tyuiu.KonevaDD.Sprint2.Task5.V5/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KonevaDD.Sprint2.Task5.V5.Lib;
+namespace Tyuiu.KonevaDD.Sprint2.Task5.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила: Конева Д. Д. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнила: Конева Дарья Денисовна | АСОиУб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите номер карты: ");
+            int numCard = Convert.ToInt32(Console.ReadLine());
+            string res;
+            if ((numCard < 6) || (numCard > 14))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "Это карта: " + ds.FindCardValue(numCard);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
